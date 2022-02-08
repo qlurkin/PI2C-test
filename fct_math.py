b734b54f1f63311178cca4f1fa905fa574ad7808
Quentin Lurkin <qlurkin@gmail.com>
2022-02-08 11:24:00 +0100

    Lazy Msg

--- fct_math.py
+++ fct_math.py
@@ -8,6 +8,9 @@ def div(a, b):
 def mult(a, b):
     return a * b
 
+def sub(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(add(3, 4))
     print(mult(3, 4))
---
+++ test_fct_math.py
@@ -0,0 +1,6 @@
+import fct_math
+
+def test_sub():
+    assert fct_math.sub(5, 4) == 1
+    assert fct_math.sub(7, 4) == 3
+    assert fct_math.sub(5, 1) == 4
